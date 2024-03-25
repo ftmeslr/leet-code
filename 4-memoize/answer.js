622f28abf5e89e445c4d190b85f595ef2e28c837
f.salarian <ftme_slr@yahoo.com>
2024-03-25 09:33:50 +0330

    add memoize function

---
+++ 4-memoize/answer.js
@@ -0,0 +1,14 @@
+function memoize(fn) {
+  const cache = {};
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache[key]) return cache[key];
+    else {
+      const result = fn(...args);
+      cache[key] = result;
+      return result;
+    }
+  };
+}
