99c8877056c1a87e6057376aa0122f84451b3eb4
unknown <ftme.slr@gmail.com>
2024-06-04 17:30:05 +0330

    add space

--- 9-RemoveDuplicatesSortedArray/answer.js
+++ 9-RemoveDuplicatesSortedArray/answer.js
@@ -1,10 +1,12 @@
 const removeDuplicates = (nums) => {
     let k = 1;
+
     for (let i = 1; i < nums.length - 1; i++) {
         if (nums[i] !== nums[i + 1]) {
             nums[k] = nums[i + 1]
             k++
         }
     }
+
     return k
 };
