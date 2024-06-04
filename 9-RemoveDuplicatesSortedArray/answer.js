bc4bf962bc781ef9e1c2f9524b130ed0c36815f2
unknown <ftme.slr@gmail.com>
2024-06-04 17:40:04 +0330

    change first step of i

--- 9-RemoveDuplicatesSortedArray/answer.js
+++ 9-RemoveDuplicatesSortedArray/answer.js
@@ -1,7 +1,7 @@
 const removeDuplicates = (nums) => {
     let k = 1;
 
-    for (let i = 1; i < nums.length - 1; i++) {
+    for (let i = 0; i < nums.length - 1; i++) {
         if (nums[i] !== nums[i + 1]) {
             nums[k] = nums[i + 1]
             k++
